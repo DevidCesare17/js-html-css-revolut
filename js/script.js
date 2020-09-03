b664fb6ffd756017fef6264db1c13df923be8f75
Davide Cesare <davidereaper@gmail.com>
2020-09-03 16:46:40 +0200

    Inizio visualizzazione menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function{
+$(document).ready(function () {
 
+  // applico colore nero al testo al click dell'utente
+  $("li > a").click(
+    function () {
+      $("li > a").removeClass("active");
+      $(this).addClass("active");
+    }
+  );
 
+  // visualizzo e nascondo menu a tendina all'hover dell'utente
+  $(this).mouseover(
+    function () {
+      $(".m_tendina_1").toggle();
+    }
+  );
 
 });
